fb2cb39264e2fc3d5a0a9d4a69334da70a687b29
diaspd <pdr050203@gmail.com>
2023-08-15 09:57:08 -0300

    added a dropdown menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 import { SettingsTabs } from '@/components/SettingsTabs'
 import Logo from '../../public/Logomark.svg'
-import { Menu } from 'lucide-react'
 import Image from 'next/image'
+import { Dropdown } from '@/components/Dropdown'
 
 export default function Home() {
   return (
     <>
       <header className="-ml-1.5 flex border-b border-b-zinc-200 pb-2 lg:hidden">
-        <Image src={Logo} width={30} height={30} alt="" />
+        <Image src={Logo} width={40} height={40} alt="" />
         <span className="ml-auto">
-          <Menu className="h-5 w-5 dark:text-zinc-300" />
+          <Dropdown />
         </span>
       </header>
       <h1 className="mt-5 text-3xl font-medium text-zinc-900 dark:text-zinc-200">
