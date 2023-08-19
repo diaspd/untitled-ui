c09406ae5d0dc09f6d16acdf130e0003abd5c544
diaspd <pdr050203@gmail.com>
2023-08-19 08:59:31 -0300

    animate select component

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,31 @@ module.exports = {
         app: 'minmax(18rem, 20rem) 1fr',
         form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
       },
+
       borderWidth: {
         6: '6px',
       },
+
       colors: {
         violet: {
           25: '#fcfaff',
         },
       },
+
       maxWidth: {
         app: '700px',
+      },
+
+      keyframes: {
+        slideDownAndFade: {
+          from: { opacity: 0, transform: 'translateY(-2px)' },
+          to: { opacity: 1, transform: 'translateY(0)' },
+        },
+      },
+
+      animation: {
+        slideDownAndFade:
+          'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
       },
     },
   },
