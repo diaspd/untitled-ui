00fc87daa5be6c19dcf6befba6a707d791dc2d64
diaspd <pdr050203@gmail.com>
2023-08-19 07:42:45 -0300

    added animation to file upload

--- src/components/HomeForm/FileInput/FileList.tsx
+++ src/components/HomeForm/FileInput/FileList.tsx
@@ -1,20 +1,23 @@
 'use client'
+
 import { useEffect } from 'react'
 import { Trash2, UploadCloud } from 'lucide-react'
 import { useFileInput } from './Root'
 import { formatBytes } from '@/utils/format-bytes'
 import { useFileUpload } from '../../../hooks/useFileUpload'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 
 export function FileList() {
   const { files } = useFileInput()
   const { progress, simulateFileUpload } = useFileUpload()
+  const [parent] = useAutoAnimate()
 
   useEffect(() => {
     simulateFileUpload()
   }, [])
 
   return (
-    <div className="mt-4 space-y-3">
+    <div ref={parent} className="mt-4 space-y-3">
       {files.map((file) => {
         return (
           <div
